d52c96022c98cb68f0ac5a81f5d921c3cc208ad7
FL3NKEY <noreply@example.com>
2023-09-20 15:22:53 +0300

    update deps

--- vue3/typescript.js
+++ vue3/typescript.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: ['./index'],
     parserOptions: {
         parser: '@typescript-eslint/parser',
-         ecmaVersion: 2020,
+        ecmaVersion: 2020,
         sourceType: 'module',
     }
 };
